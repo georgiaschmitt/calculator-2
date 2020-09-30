27013905c2ae2c2a23852c5c8aa55281f66997b3
vagrant <vagrant@vagrant.vm>
2020-09-30 20:32:25 +0000

    Added subtract function call

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                        power, mod, )
 while True:
-    equation = input("Enter the equation")
+    equation = input("Enter the equation ")
     tokens = equation.split(' ') 
     if "q" in tokens:
         print("You will exit.")
@@ -12,4 +12,7 @@ while True:
     else:
         if tokens[0] == '+':
             result = add(float(tokens[1]), float(tokens[2]))
-        print(result)
+        if tokens[0] == '-':
+            result = subtract(float(tokens[1]), float(tokens[2]))
+    
+    print(result)
