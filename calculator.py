b352d38c72e40b2c0766049a97bb39a2e9e30639
vagrant <vagrant@vagrant.vm>
2020-09-30 20:08:10 +0000

    Take input, tokenize, and quit option added

--- calculator.py
+++ calculator.py
@@ -1,7 +1,12 @@
 """CLI application for a prefix-notation calculator."""
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
-                        power, mod, )
-
+                       power, mod, )
+while True:
+    equation = input("Enter the equation")
+    tokens = equation.split(' ') 
+    if "q" in tokens:
+        print("You will exit.")
+        break
 
 # Replace this with your code
